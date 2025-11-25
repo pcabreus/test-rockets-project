a26690b186d358f1c59070bdcbaaa9494f27f5ca
Pedro C. Charly Abreu <pcabreus@gmail.com>
2025-11-25 14:51:45 +0100

    Cleen up some detailt for manual testing

--- internal/consumer/event.go
+++ internal/consumer/event.go
@@ -8,14 +8,18 @@ import (
 	"github.com/pcabreus/test-rockets-project/internal/model"
 )
 
+const firstExpectedEventNumber = 1
+
 type EventConsumer interface {
 	Start(ctx context.Context) error
 	Consume(ctx context.Context, event model.Event) error
 }
 
+// RocketEventConsumer processes rocket events in order per channel
+// It was designed for only one instance running.
 type RocketEventConsumer struct {
 	eventStore model.EventStore
-	next       map[string]int // track next expected message number per channel
+	next       map[string]int // track next expected message number per channel.
 }
 
 func NewRocketEventConsumer(eventStore model.EventStore) *RocketEventConsumer {
@@ -25,13 +29,13 @@ func NewRocketEventConsumer(eventStore model.EventStore) *RocketEventConsumer {
 	}
 }
 
-// Start the  consumer (no-op for in-memory)
+// Start the consumer
 // Open a goroutine pending events
 func (c *RocketEventConsumer) Start(ctx context.Context) error {
 	log.Println("RocketEventConsumer started")
 
 	go func() {
-		ticker := time.NewTicker(200 * time.Millisecond)
+		ticker := time.NewTicker(1 * time.Second)
 		defer ticker.Stop()
 
 		for {
@@ -40,6 +44,7 @@ func (c *RocketEventConsumer) Start(ctx context.Context) error {
 				log.Println("RocketEventConsumer stopped")
 				return
 			case <-ticker.C:
+				log.Println("Checking for pending events...")
 				pending, err := c.eventStore.Pending(ctx)
 				if err != nil {
 					log.Println("error listing pending events:", err)
@@ -51,8 +56,9 @@ func (c *RocketEventConsumer) Start(ctx context.Context) error {
 					num := ev.Number
 
 					// default first expected number is 1
+					// we are to using mutex here assuming single instance
 					if _, ok := c.next[ch]; !ok {
-						c.next[ch] = 1
+						c.next[ch] = firstExpectedEventNumber
 					}
 
 					// only process if it matches the next expected number
@@ -60,6 +66,7 @@ func (c *RocketEventConsumer) Start(ctx context.Context) error {
 						if err := c.Consume(ctx, ev); err != nil {
 							log.Println("consume error:", err)
 							// leave next unchanged so it will be retried later
+							// go to a dead-letter queue after some retries
 							continue
 						}
 						c.next[ch]++
